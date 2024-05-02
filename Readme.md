bdeb9b929b50ac19bac18126af5aefb7b2551b8b
Tushar Mistry <mistry.tushar98@gmail.com>
2024-05-02 11:04:59 +0530

    fix(install): update install script
     - Non sudo installation install to user's bin folder
     - Update readme to add platform support information

--- Readme.md
+++ Readme.md
@@ -1,7 +1,18 @@
 ## Rust CLI app to find keywords of a file in directory
 
+### Platform Support
+Currently support only for 
+- aarch64-apple-darwin
+
+Future support for 
+- aarch64-unknown-linux-gnu
+- x86_64-apple-darwin
+- x86_64-unknown-linux-gnu
+
 ### Installation
-curl -s https://raw.githubusercontent.com/tushar1998/rust-cli-find-line/v0.0.2/install.sh | bash
+```bash
+curl -s https://raw.githubusercontent.com/tushar1998/rust-cli-find-line/v0.0.3-dev.0/install.sh | bash
+```
 
 ### Usage
 
--- install.sh
+++ install.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+update_path() {
+    local profile_file="$1"
+    if [ -f "$profile_file" ] && ! grep -qx 'export PATH="$HOME/bin:$PATH"' "$profile_file"; then
+        echo 'export PATH="$HOME/bin:$PATH"' >> "$profile_file"
+        echo "Updated $profile_file with PATH settings."
+    fi
+}
+
 # Define the URL to your zipped binary on GitHub Releases
-BINARY_URL="https://github.com/tushar1998/rust-cli-find-line/releases/download/v0.0.2/findline"
-INSTALL_PATH="/usr/local/bin"
+BINARY_URL="https://github.com/tushar1998/rust-cli-find-line/releases/download/v0.0.3-dev.0/findline"
+INSTALL_PATH="/Users/$USER/bin"
 BIN_NAME="findline"
 
 echo "Downloading findline..."
-sudo curl -L $BINARY_URL -o "$INSTALL_PATH/$BIN_NAME"
+curl -L $BINARY_URL -o "$INSTALL_PATH/$BIN_NAME"
+
+mkdir $INSTALL_PATH
 
 echo "Setting executable permissions..."
-sudo chmod +x "$INSTALL_PATH/$BIN_NAME"
+chmod +x "$INSTALL_PATH/$BIN_NAME"
+
+update_path "$HOME/.bashrc"
+update_path "$HOME/.zshrc"
 
 if [[ -f "$INSTALL_PATH/$BIN_NAME" ]]; then
-    echo "Installation successful, your-app is now available."
+    echo "Installation successful, findline is now available."
 else
     echo "Installation failed. Please check permissions and internet connection."
 fi
